44505c379c4d46ef5dc943f948379d3b9d645369
99061142 <99061142@mydavinci.nl>
2021-05-28 16:15:36 +0200

    School project
    Added:
    
    Everything the school provided.
    Functions to connect to the database.
    Function to change the employee.

---
+++ medewerkers_in_mvc/view/employee/update.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+
+<h1><?= $data["name"] ?> wijzigen</h1>
+<p>Als u een vak leeg laat vult hij automatich niks in</h1>
+<h1><?= $data["name"] ?> wijzigen</h1>
+<form name="update" method="post" action="<?=URL?>employee/update">
+	<input type="hidden" name="id" value="<?= $data["id"] ?>">
+	Name: <br><input class="my-3" type="text" name="name"><br>
+	Age: <br><input class="mt-3" type="text" name="age"><br>
+	<input class="mt-5" type="submit" name="submit">
+</form>
---
+++ medewerkers_in_mvc/view/templates/footer.php
@@ -0,0 +1,4 @@
+		<footer>&copy; Xander Briem</footer>
+		</div><!-- end container div -->
+	</body>
+</html>
